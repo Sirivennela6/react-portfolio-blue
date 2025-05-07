36134195b4456792274f15f75ec0296c9bbec669
Siri Vennela <sirivennela6166@gmail.com>
2025-05-07 09:48:18 +0530

    fifth

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import  './App.css';
+
 
 function App() {
   return (
