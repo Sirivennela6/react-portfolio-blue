2674a7cb9409104dc66e01684f54ebf23b26867b
Siri Vennela <sirivennela6166@gmail.com>
2025-05-08 10:11:28 +0530

    sixth

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import  './App.css';
+import ContactForm from './pages/ContactForm';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<ContactForm />} />
       </Routes>
       <Footer />
     </Router>
